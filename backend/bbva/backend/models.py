4c3c0c096175b3d533391abfed29868ce31457fd
io0_0 <elvis.tellez.1234@gmail.com>
2019-07-19 12:37:59 -0500

    agregando Servicios

--- backend/bbva/backend/models.py
+++ backend/bbva/backend/models.py
@@ -13,7 +13,7 @@ class Cliente(models.Model):
     class Meta:
         db_table = 'cliente'
     def __str__(self):
-        self.nombres
+        return self.nombres
 
 class Servicio(models.Model):
     id_servicio = models.AutoField(primary_key=True)
@@ -21,17 +21,24 @@ class Servicio(models.Model):
 
     class Meta:
         db_table = 'servicio'
-
+    def __str__(self):
+        return self.nombres
 
 class Cuenta(models.Model):
     id_cuenta = models.IntegerField(primary_key=True)
     id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     id_servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
     monto = models.DecimalField(decimal_places=2, max_digits=8)
-    estado = models.CharField(max_length=15)
+    ESTADOS = [
+        (1, 'Pagado'),
+        (2, 'No Pagado'),
+        ]
+
+    estado = models.CharField(max_length=15,choices = ESTADOS)
     fecha_vencimiento = models.DateField()
     fecha_pago = models.DateField()
 
     class Meta:
         db_table = 'cuenta'
-
+    def __str__(self):
+        return str(self.id_cliente)+ str(self.id_servicio)
